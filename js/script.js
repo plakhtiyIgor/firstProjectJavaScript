07068964d82e613742e251bee91478d5b8404cdb
Igor <plakhtiy.igor@gmail.com>
2022-04-03 22:22:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+// let number = 4.6;
+
+// console.log(-4 / 0);
+// console.log("string" * 3);
+
+// const persone = "Alex";
+// const boll = true;
+
+// console.log(somthing);
+
+// let box;
+// console.log(box);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// let arr = ["plum.png", "orange.jpg", "apple.bmp", 6, true, {}, []];
+// console.log(arr[1]);
+
+// alert("Hello!");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", " ");
+// answers[1] = prompt("Как вашa фамилия?", " ");
+// answers[2] = +prompt("Сколько вам лет", " ");
+// document.write(answers);
+
+//8.Интерполяция
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Hello, ${user}!`);
+
+//9.Операторы
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+// console.log(4 + +"5");
+// console.log(5 + 10);
+
+// let incr = 10, decr = 10;
+//постфиксная форма записи
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
+//префиксная форма записи
+// ++incr;
+// --incr;
+// console.log(incr);
+// console.log(decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5 % 2);
+// console.log(2*4 == 8);
+// console.log(2*4 == "8");
+// console.log(2*4 === "8");
+
+console.log(true || false);
+console.log(true && false);
+
+const isChecked = true;
+const isClose = true;
+console.log(isChecked && isClose);
+console.log(isChecked && !isClose);
